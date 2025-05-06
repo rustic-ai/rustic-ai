b85d36bc8dee27796c2afed9d1de866911ab5d87
Shiti Saxena <shiti@dragonscale.ai>
2025-05-05 18:22:24 -0700

    ci: add gcsfs as optional dependency and fix pr validate on macos

--- scripts/poetry_install.sh
+++ scripts/poetry_install.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "${DIR}/.." || exit
 
 poetry self add poetry-plugin-mono-repo-deps@0.3.2
-poetry install --without dev --sync
+poetry install --without dev --all-extras --sync
 
