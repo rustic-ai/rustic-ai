6f96af1fc2c084d7c57ecc30fe99060c8e5812a2
Shiti Saxena <shiti@dragonscale.ai>
2025-07-10 15:19:54 -0700

    fix(ray): set num_cpus for agents

--- api/src/rustic_ai/api_server/catalog/catalog_store.py
+++ api/src/rustic_ai/api_server/catalog/catalog_store.py
@@ -372,7 +372,7 @@ class CatalogStore:
                     GuildModel.name,
                     Blueprint.id.label("blueprint_id"),  # type:ignore
                     Blueprint.icon,
-                    GuildModel.status
+                    GuildModel.status,
                 )
                 .select_from(GuildModel)
                 .outerjoin(BlueprintGuild, GuildModel.id == BlueprintGuild.guild_id)  # type:ignore
@@ -414,7 +414,7 @@ class CatalogStore:
                     GuildModel.name,
                     Blueprint.id.label("blueprint_id"),  # type:ignore
                     Blueprint.icon,
-                    GuildModel.status
+                    GuildModel.status,
                 )
                 .select_from(
                     join(GuildModel, UserGuild, GuildModel.id == UserGuild.guild_id)  # type:ignore
