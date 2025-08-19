7999fd59529086952813c5c31f813b941be35523
Rohit Rai <milliondreams@users.noreply.github.com>
2025-08-19 11:44:55 -0700

    chore: minor formatiing and typo

--- llm-agent/src/rustic_ai/llm_agent/__init__.py
+++ llm-agent/src/rustic_ai/llm_agent/__init__.py
@@ -1,5 +1,11 @@
-from .agent import LLMAgent
-from .llm_agent_conf import LLMAgentConfig
+from .basic_llm_agent import BasicLLMAgent, BasicLLMAgentConfig
+from .llm_agent_conf import LLMAgentConfig, LLMConfigBase
 from .llm_agent_utils import LLMAgentUtils
 
-__all__ = ["LLMAgent", "LLMAgentConfig", "LLMAgentUtils"]
+__all__ = [
+    "BasicLLMAgent",
+    "BasicLLMAgentConfig",
+    "LLMAgentConfig",
+    "LLMConfigBase",
+    "LLMAgentUtils",
+]
