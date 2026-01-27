e3440d7be57b659776aa033632e54d2c3e1d8b9a
Shiti Saxena <shiti@dragonscale.ai>
2026-01-27 10:22:09 -0800

    fix: change structured_output plugin to preprocess and add the required format automatically

--- llm-agent/src/rustic_ai/llm_agent/plugins/structured_output.py
+++ llm-agent/src/rustic_ai/llm_agent/plugins/structured_output.py
@@ -11,23 +11,25 @@ from rustic_ai.core.guild.agent_ext.depends.llm.models import (
     ChatCompletionResponse,
 )
 from rustic_ai.core.utils.basic_class_utils import get_class_from_name
-from rustic_ai.llm_agent.plugins.response_postprocessor import ResponsePostprocessor
+from rustic_ai.llm_agent import LLMCallWrapper
 
 
-class StructuredOutputPlugin(ResponsePostprocessor):
+class StructuredOutputPlugin(LLMCallWrapper):
+    output_format_class_name: str
+
+    def preprocess(self, agent: Agent, ctx: ProcessContext[ChatCompletionRequest], request: ChatCompletionRequest,
+                   llm: LLM) -> ChatCompletionRequest:
+        return request.model_copy(update={"response_format": self.output_format_class_name})
+
     def postprocess(self, agent: Agent, ctx: ProcessContext[ChatCompletionRequest], final_prompt: ChatCompletionRequest,
                     llm_response: ChatCompletionResponse, llm: LLM) -> Optional[List[BaseModel]]:
-        output_format = ctx.payload.response_format
-        if isinstance(output_format, str):
-            format_type = get_class_from_name(output_format)
-            # If it's a Pydantic model class, convert to JSON schema
-            if isinstance(format_type, type) and issubclass(format_type, BaseModel):
-                result = []
-                for choice in llm_response.choices:
-                    content = json.loads(choice.message.content)
-                    result.append(format_type.model_validate(content))
-                return result
-            else:
-                raise ValueError(f"Unsupported response format: {output_format}")
+        format_type = get_class_from_name(self.output_format_class_name)
+        # If it's a Pydantic model class, convert to JSON schema
+        if isinstance(format_type, type) and issubclass(format_type, BaseModel):
+            result = []
+            for choice in llm_response.choices:
+                content = json.loads(choice.message.content)
+                result.append(format_type.model_validate(content))
+            return result
         else:
-            raise ValueError(f"Invalid response format type: {type(output_format)}")
+            raise ValueError(f"Unsupported response format: {self.output_format_class_name}")
